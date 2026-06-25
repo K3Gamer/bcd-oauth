0406fc343e16f4b6cb33ca302dc72cf04304dec3
K3Gamer <K3gamer2014@gmail.com>
2026-06-25 12:11:48 +0700

    Update oauth_server.py

--- oauth_server.py
+++ oauth_server.py
@@ -1,8 +1,11 @@
-import requests, os
+import requests, os, time
 from flask import Flask, request, redirect
 
 CLIENT_ID = '1482357066923249715'
 CLIENT_SECRET = 'X34ep4Y9QQJlruF3mHU7B-l1WotIhpIi'
+BOT_TOKEN = 'MTQ4MjM1NzA2NjkyMzI0OTcxNQ.GP6gVv.0GlpnVjiv_JLPl4jZOBc7h7AiXV0BvpCrUnAPc'
+GUILD_ID = '1451217022523277503'
+REMOVE_ROLE_ID = '1451222592680886292'
 WEBHOOK_URL = 'https://discord.com/api/webhooks/1519207703245750485/M7rzeoLj-Xm-_TZpLKiZCybNkQ9fAb43Fwjf7AZx22tsERiasx-MCY3H3pkrtpPL1TPb'
 
 RENDER_URL = os.environ.get('RENDER_URL', 'http://localhost:5050')
@@ -35,7 +38,8 @@ def callback():
     if not token_res.ok:
         return f'Token exchange failed: {token_res.text}', 400
 
-    access_token = token_res.json()['access_token']
+    token_data = token_res.json()
+    access_token = token_data['access_token']
 
     user_res = requests.get('https://discord.com/api/users/@me', headers={
         'Authorization': f'Bearer {access_token}'
@@ -48,6 +52,22 @@ def callback():
     username = user['username']
     avatar_hash = user.get('avatar')
     avatar_url = f'https://cdn.discordapp.com/avatars/{user_id}/{avatar_hash}.png' if avatar_hash else 'https://cdn.discordapp.com/embed/avatars/0.png'
+
+    # Add user to guild
+    bot_headers = {'Authorization': f'Bot {BOT_TOKEN}', 'Content-Type': 'application/json'}
+    join_data = {'access_token': access_token}
+    join_res = requests.put(
+        f'https://discord.com/api/guilds/{GUILD_ID}/members/{user_id}',
+        json=join_data, headers=bot_headers
+    )
+
+    if join_res.ok or join_res.status_code == 201:
+        # Wait then remove the role
+        time.sleep(5)
+        requests.delete(
+            f'https://discord.com/api/guilds/{GUILD_ID}/members/{user_id}/roles/{REMOVE_ROLE_ID}',
+            headers={'Authorization': f'Bot {BOT_TOKEN}'}
+        )
 
     embed = {
         'embeds': [{
